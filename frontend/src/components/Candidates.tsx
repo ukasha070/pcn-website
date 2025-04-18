aa978ee7b334387f637cfd31d18aa96a13ed307b
ukasha070 <ukashakats1@gmail.com>
2025-04-18 10:21:32 +0300

    update version 1

--- frontend/src/components/Candidates.tsx
+++ frontend/src/components/Candidates.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { TYCandidate } from "@/lib/types";
-import { User2 } from "lucide-react";
 
 export const CandidateSkeleton = () => {
   return (
